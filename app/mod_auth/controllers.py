b9c22c0bd439fe853854a5843fa1a479688c7979
Ridho Febriansa <officialridhof@gmail.com>
2020-04-03 16:39:53 +0700

    add simple implementation of auth model

--- app/mod_auth/controllers.py
+++ app/mod_auth/controllers.py
@@ -1,7 +1,13 @@
 """
 Auth Module's Controllers
 """
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, render_template, request
+
+from app import DB as db
+
+from app.mod_auth.forms import LoginForm
+
+from app.mod_auth.models import User
 
 MOD_AUTH = Blueprint('auth', __name__, url_prefix='/')
 
@@ -11,7 +17,14 @@ def login():
     """
     Return a HTML of Login Page.
     """
-    return render_template("auth/login.html")
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(name=form.username.data).first()
+        if user:
+            flash('Welcome')
+        flash('Username atau Password tidak ditemukan.')
+    return render_template("auth/login.html", form=form)
+
 
 @MOD_AUTH.route('/register', methods=['GET', 'POST'])
 def register():
