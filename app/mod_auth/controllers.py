e8e31c0d30044c6336d3754c72777f9165b1275b
Ridho Febriansa <officialridhof@gmail.com>
2020-04-03 17:52:29 +0700

    save data to db on regis and store to session on login, use proper flash

--- app/mod_auth/controllers.py
+++ app/mod_auth/controllers.py
@@ -1,7 +1,7 @@
 """
 Auth Module's Controllers
 """
-from flask import Blueprint, flash, render_template, request
+from flask import Blueprint, flash, redirect, render_template, request, session, url_for
 
 from app import DB as db
 
@@ -21,8 +21,10 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(name=form.username.data).first()
         if user:
-            flash('Welcome')
-        flash('Username atau Password tidak ditemukan.')
+            if user.check_password(form.password.data):
+                session['username'] = user.name
+                return redirect(url_for('landing_page.landing_page'))
+        flash('Username atau Password tidak ditemukan.', 'danger')
     return render_template("auth/login.html", form=form)
 
 
@@ -31,4 +33,27 @@ def register():
     """
     Return a HTML of Register Page.
     """
-    return render_template("auth/register.html")
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(name=form.username.data).first()
+        if not user:
+            user = User(form.username.data, form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            flash(
+                'User %r telah berhasil didaftarkan.' %
+                (user.name), 'success')
+        else:
+            flash(
+                'Username %r telah terdaftar.' %
+                (user.name), 'danger')
+    return render_template("auth/register.html", form=form)
+
+@MOD_AUTH.route('/logout', methods=['GET'])
+def logout():
+    """
+    Do logout (erase a session).
+    """
+    session['username'] = None
+    flash('Berhasil logout', 'info')
+    return redirect(url_for('auth.login'))
