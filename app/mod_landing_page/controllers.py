19985bba8ddc6dd24fae634e7fbd237301e55b41
Ridho Febriansa <officialridhof@gmail.com>
2020-04-03 14:00:55 +0700

    add endpoint to return robots file

--- app/mod_landing_page/controllers.py
+++ app/mod_landing_page/controllers.py
@@ -1,7 +1,7 @@
 """
 Landing Page Module's Controllers
 """
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 
 MOD_LANDING_PAGE = Blueprint('landing_page', __name__, url_prefix='/')
 
@@ -12,3 +12,10 @@ def landing_page():
     Return a HTML of Landing Page.
     """
     return render_template("landing_page/landing_page.html")
+
+@MOD_LANDING_PAGE.route('/robots.txt', methods=['GET', 'POST'])
+def robots():
+    """
+    Return robot.txt file.
+    """
+    return send_from_directory('static/', filename='robots.txt')
