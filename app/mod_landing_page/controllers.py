80ff6b2e82f0d38e0939bcb67a54c33af053530b
Ridho Febriansa <officialridhof@gmail.com>
2020-04-03 16:40:20 +0700

    import tweet models

--- app/mod_landing_page/controllers.py
+++ app/mod_landing_page/controllers.py
@@ -3,6 +3,8 @@ Landing Page Module's Controllers
 """
 from flask import Blueprint, render_template, send_from_directory
 
+from app.mod_tweet.models import Tweet
+
 MOD_LANDING_PAGE = Blueprint('landing_page', __name__, url_prefix='/')
 
 
@@ -11,7 +13,8 @@ def landing_page():
     """
     Return a HTML of Landing Page.
     """
-    return render_template("landing_page/landing_page.html")
+    tweet = Tweet.query.all()
+    return render_template("landing_page/landing_page.html", tweet=tweet)
 
 @MOD_LANDING_PAGE.route('/robots.txt', methods=['GET', 'POST'])
 def robots():
