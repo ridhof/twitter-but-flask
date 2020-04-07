ac86b17ea377469b8b18dff6df2c4e1030c5f6a8
Ridho Febriansa <officialridhof@gmail.com>
2020-04-07 16:47:11 +0700

    add rest api example of retrieving tweets

--- app/mod_landing_page/controllers.py
+++ app/mod_landing_page/controllers.py
@@ -10,6 +10,8 @@ from app.mod_tweet.forms import TweetForm
 from app.mod_tweet.models import Tweet
 from app.mod_auth.models import User
 
+from common import pretty_result, code
+
 MOD_LANDING_PAGE = Blueprint('landing_page', __name__, url_prefix='/')
 
 
@@ -28,6 +30,21 @@ def landing_page():
         form.tweet.data = ""
     tweets = Tweet.query.order_by(Tweet.id.desc()).limit(5)
     return render_template("landing_page/landing_page.html", username=username, tweets=tweets, form=form)
+
+@MOD_LANDING_PAGE.route('/tweet', methods=['GET'])
+def get_tweet():
+    """
+    Return tweets
+    """
+    query_tweets = Tweet.query.order_by(Tweet.id.desc()).limit(5)
+    tweets = []
+    for query_tweet in query_tweets:
+        tweets.append({
+            'tweet': query_tweet.text, 
+            'username': query_tweet.get_username(),
+            'user_id': query_tweet.user_id,
+        })
+    return pretty_result(code.OK, data=tweets)
 
 @MOD_LANDING_PAGE.route('/robots.txt', methods=['GET', 'POST'])
 def robots():
